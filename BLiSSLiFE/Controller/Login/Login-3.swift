769e3ed2d266527f31a41b5e6b00bfb4a29e1b73
tushar1210 <ts26192271@gmail.com>
2019-12-09 12:16:26 +0530

    Alamofire fix

--- BLiSSLiFE/Controller/Login/Login-3.swift
+++ BLiSSLiFE/Controller/Login/Login-3.swift
@@ -16,16 +16,34 @@ class Login_3: UIViewController {
     @IBOutlet weak var userNameTF: UITextField!
     @IBOutlet weak var passwordTF: UITextField!
     @IBOutlet weak var login: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         bgView.roundCorners([.bottomLeft], radius: 80)
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+
+    @IBAction func login(_ sender: Any) {
+        let params = ["email":userNameTF.text,"password":passwordTF.text]
+        let url = URL(string: "https://blisslife.herokuapp.com/users/login")
+        var request = URLRequest(url: url!)
+        request.httpMethod = HTTPMethod.post.rawValue
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        let data:Data = NSKeyedArchiver.archivedData(withRootObject: params)
+        request.httpBody = data
+        Alamofire.request(request).responseJSON { (response) in
+            print(response.data!)
+        }
+        
+//        Alamofire.request(url!, method: .post, parameters: params).responseJSON {
+//            response in
+//            print(response.value!)
+//        }
         
     }
     
 
-    @IBAction func login(_ sender: Any) {
-    }
-    
-
 }
+
