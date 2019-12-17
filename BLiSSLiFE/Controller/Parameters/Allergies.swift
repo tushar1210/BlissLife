4462cd743f0db94e04c47eab2907df6fd459a15e
tushar1210 <ts26192271@gmail.com>
2019-12-18 00:15:53 +0530

    ui

---
+++ BLiSSLiFE/Controller/Parameters/Allergies.swift
@@ -0,0 +1,23 @@
+//
+//  Allergies.swift
+//  BLiSSLiFE
+//
+//  Created by Tushar Singh on 17/12/19.
+//  Copyright © 2019 Tushar Singh. All rights reserved.
+//
+
+import UIKit
+
+class Allergies: UIViewController {
+
+    @IBOutlet weak var bgView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        bgView.roundCorners([.bottomLeft], radius: 80)
+    }
+    
+
+    
+
+}
