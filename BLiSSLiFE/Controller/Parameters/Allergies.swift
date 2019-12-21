f94fcefb70f483e15e3dab40835939feb0b2ba16
tushar1210 <ts26192271@gmail.com>
2019-12-21 23:25:21 +0530

    allergies

--- BLiSSLiFE/Controller/Parameters/Allergies.swift
+++ BLiSSLiFE/Controller/Parameters/Allergies.swift
@@ -11,13 +11,36 @@ import UIKit
 class Allergies: UIViewController {
 
     @IBOutlet weak var bgView: UIView!
+    @IBOutlet weak var dustLabel: UITextView!
+    @IBOutlet weak var pollenLabel: UITextView!
+    @IBOutlet weak var peanutsLabel: UITextView!
+    @IBOutlet weak var soyLabel: UITextView!
+    @IBOutlet weak var otherTF: UITextField!
+    @IBOutlet weak var dustButton: UIButton!
+    @IBOutlet weak var pollenButton: UIButton!
+    @IBOutlet weak var peanutsButton: UIButton!
+    @IBOutlet weak var soyButton: UIButton!
+    @IBOutlet weak var otherButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         bgView.roundCorners([.bottomLeft], radius: 80)
+        dustLabel.layer.borderWidth=1
+        dustLabel.layer.borderColor = UIColor.black.cgColor
+        
     }
     
 
     
-
+    @IBAction func otherButton(_ sender: Any) {
+    }
+    @IBAction func soyButton(_ sender: Any) {
+    }
+    @IBAction func peanutsButton(_ sender: Any) {
+    }
+    @IBAction func pollenButton(_ sender: Any) {
+    }
+    @IBAction func dustButton(_ sender: Any) {
+    }
+    
 }
