9549b6724c2b074f9f2c7f27e16fe285e496efd6
tushar.1210 <ts26192271@gmail.com>
2020-02-15 13:07:52 +0530

    added recipe

---
+++ BLiSSLiFE/Controller/Food/ReciepeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ReciepeViewController.swift
+//  BLiSSLiFE
+//
+//  Created by Tushar Singh on 15/02/20.
+//  Copyright © 2020 Tushar Singh. All rights reserved.
+//
+
+import UIKit
+
+class ReciepeViewController: UIViewController {
+
+    @IBOutlet weak var reciepeImage: UIImageView!
+    @IBOutlet weak var reciepeName: UILabel!
+    @IBOutlet weak var recipeAuthor: UILabel!
+    @IBOutlet weak var reciepeTextView: UITextView!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var servingLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+   
+
+}
